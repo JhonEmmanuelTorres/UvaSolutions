78d9db9c4496da079a8384c893e53845aca1bdb4
JhonEmmanuelTorres <jhetorresto@unal.edu.co>
2016-01-05 17:11:38 -0500

    Update isPrime.cpp

--- isPrime.cpp
+++ isPrime.cpp
@@ -7,13 +7,14 @@ using namespace std;
 
 bool isPrime( int n ) // O ( sqrt(n) / 2 )
 {
-    if( n % 2 == 0 || n == 1 ) return false;
-    int limit = (int)sqrt(n);
+    if( n % 2 == 0 && n != 2 || n <= 1 ) return false;
+    int limit = (int)sqrt(n) + 1;
     for( int i = 3; i < limit; i+=2 )
         if( n % i == 0 )
             return false;
     return true;
 }
+
 
 int main()
 {
