d1dbf04212457b98e7c24fde291dd7bbe81e6fb1
JhonEmmanuelTorres <jhetorresto@unal.edu.co>
2016-01-05 13:49:48 -0500

    Create Sieve of Eratosthenes.cpp

---
+++ Sieve of Eratosthenes.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <bitset>
+
+using namespace std;
+
+void generePrimes( int n )
+{
+    vector<bool> sieve( n, true );
+    vector<int> primes;
+    sieve[0] = sieve[1] = 0;
+    for( int i = 2; i <= n; i++ )
+        if( sieve[i] )
+        {
+            for( int j = i * i; j <= n; j+=i )
+                sieve[j] = 0;
+            primes.push_back( i );
+        }
+    for( int i: primes )
+        cout << i << " ";
+    cout << endl;
+}
+
+int main()
+{
+    int n;
+    do{
+        cin >> n;
+        generePrimes(n);
+    }while( n != 0 );
+
+    return 0;
+}
